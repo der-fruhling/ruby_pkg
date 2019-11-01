a655ad8d0791e304171345ae1a78cbf9eae8dfe9
LiamCoal <liam@liamiam.com>
2019-11-01 14:13:18 -0700

    Bump version up.

--- lib/ruby_pkg/version.rb
+++ lib/ruby_pkg/version.rb
@@ -1,8 +1,8 @@
 module RubyPkg
-    VERSION = "1.1.3"
+    VERSION = "1.1.4"
     DESC = <<HEREDOC
 ruby_pkg is a simple package manager. You can use it to install and remove packages.
 It also supports the tar format compressed with either xz or gz. The default is xz.
 HEREDOC
-    WHATS_NEW = "Add better support for a place full of packages"
+    WHATS_NEW = "Add dedicated server."
 end
